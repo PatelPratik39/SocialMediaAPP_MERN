ee67736f2f946550a4fac8629b7a4b8bb4cc13c3
PatelPratik39 <ptl.pratik717@gmail.com>
2024-04-20 17:26:45 -0500

    post is pending

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,59 @@
 import bcrypt from "bcrypt";
-import jwt from 'jsonwebtoken';
-import User from "../models/User.js"
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+/************** REGISTER USER **************/
+
+export const register = async (req, res) => {
+  try {
+    // destructuring
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation
+    } = req.body;
+
+    const salt = await bcrypt.genSalt(); // salt used to encrypt the password
+    const hashPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashPassword,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProgile: Math.floor(Math.random() * 10000),
+      impression: Math.floor(Math.random() * 10000)
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/**************  LOGIN IN **************/
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user)
+      return res.status(400).json({ message: "User does not exist!!" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid password" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password; // this line make sure that pssword can not render on frontEnd
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
