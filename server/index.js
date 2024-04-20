bd7087deb085c6865164871172f7d700600de989
PatelPratik39 <ptl.pratik717@gmail.com>
2024-04-20 15:54:08 -0500

    server and Auth and Model

--- server/index.js
+++ server/index.js
@@ -8,3 +8,45 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
+
+/*  CONFIGURATION */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+// multer is used to upload images and files
+const upload = multer({ storage });
+
+/* ROUTES with files  */
+app.post("auth/register", upload.single("picture"), register);
+
+// Mongoose Setup
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on PORT : ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect!!`));
