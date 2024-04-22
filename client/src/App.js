dc5593be9572789b669f100968f5d36ebdb0f6e1
PatelPratik39 <ptl.pratik717@gmail.com>
2024-04-22 00:08:29 -0500

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./scenes/homePage/index.jsx";
+import LoginPage from "./scenes/loginPage/index.jsx";
+import ProfilePage from "./scenes/profilePage/index.jsx";
 
 function App() {
-  return <div className="App">Hello Social Media App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
